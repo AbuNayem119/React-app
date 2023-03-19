0f2879b9ff53ce1e4be2b82cd8be04ec115f2c5b
Md. Abu Nayem <abunayem119@gmail.com>
2023-03-19 11:00:26 +0600

    third Commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
-import About from './Pages/About/About';
-import Contact from './Pages/Contact/Contact';
 import Home from './Pages/Home/Home';
 
 
 function App() {
   return (
     <>
-      <Home></Home>
+      <Home mas="Hello Bangladesh" nam="My country is BD">
+      </Home>
     </>
   );
 }
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 
-const Home = () => {
+const Home = (mass) => {
   return (
     <>
         <div className="container">
             <div className="row">
                 <div className="col">
-                    <h2>React Home Page</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo modi id aperiam nostrum nobis ratione quam sint. Unde, vero totam?</p>
+                    <h2>{mass.mas}</h2>
+                    <p>{mass.nam}</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos iste dignissimos temporibus fugit obcaecati harum deleniti quasi magnam iure, non vero neque id debitis facilis cupiditate mollitia commodi, nemo illo!</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos iste dignissimos temporibus fugit obcaecati harum deleniti quasi magnam iure, non vero neque id debitis facilis cupiditate mollitia commodi, nemo illo!</p>
                 </div>
             </div>
         </div>
